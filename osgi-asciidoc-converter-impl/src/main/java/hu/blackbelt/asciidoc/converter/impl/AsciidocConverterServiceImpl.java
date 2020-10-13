15e2fc65c6f94f139cac17c6d5d7c68bfb41047c
Robert Csakany <csakany.robert@gmail.com>
2020-10-13 11:34:48 +0200

    Not workinng - Suspended - Classloader issues have to be resolved.

--- osgi-asciidoc-converter-impl/src/main/java/hu/blackbelt/asciidoc/converter/impl/AsciidocConverterServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package hu.blackbelt.asciidoc.converter.impl;
-
-import hu.blackbelt.asciidoc.converter.api.AsciidocConverterService;
-
-public class AsciidocConverterServiceImpl implements AsciidocConverterService {
-}
