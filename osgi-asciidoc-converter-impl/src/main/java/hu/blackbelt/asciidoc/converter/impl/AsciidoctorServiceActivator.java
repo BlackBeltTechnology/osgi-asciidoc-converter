9548d5de7b970c6ebd1e06de0083bf1be4b21d85
Robert Csakany <csakany.robert@gmail.com>
2021-09-19 00:31:55 +0200

    Registering Asciidoctor as a service and OSGi tests added.

--- osgi-asciidoc-converter-impl/src/main/java/hu/blackbelt/asciidoc/converter/impl/AsciidoctorServiceActivator.java
+++ osgi-asciidoc-converter-impl/src/main/java/hu/blackbelt/asciidoc/converter/impl/AsciidoctorServiceActivator.java
@@ -1,78 +1,30 @@
 package hu.blackbelt.asciidoc.converter.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.sling.commons.classloader.DynamicClassLoaderManager;
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.Options;
-import org.jruby.Ruby;
-import org.jruby.RubyInstanceConfig;
 import org.jruby.embed.osgi.OSGiScriptingContainer;
-import org.jruby.javasupport.JavaEmbedUtils;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-
-import java.net.URL;
-import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.Vector;
+
+import static org.asciidoctor.jruby.AsciidoctorJRuby.Factory.create;
 
 @Component(immediate = true)
 @Slf4j
 public class AsciidoctorServiceActivator {
 
     ServiceRegistration serviceRegistration;
-    Ruby runtime;
-
-    @Reference
-    DynamicClassLoaderManager dynamicClassLoaderManager;
-
+    OSGiScriptingContainer container;
     @Activate
     public void activate(BundleContext bundleContext) {
+        container = new OSGiScriptingContainer(bundleContext.getBundle());
+        Asciidoctor asciidoctor = create(container.getOSGiBundleClassLoader());
+        log.info("Init AsciiDoctorJ version: " + asciidoctor.asciidoctorVersion());
 
-//        final OSGiScriptingContainer container = new OSGiScriptingContainer(bundleContext.getBundle());
-//        container.addClassLoader(dynamicClassLoaderManager.getDynamicClassLoader());
-//        container.addClassLoader(Asciidoctor.class.getClassLoader());
-//        container.addClassLoader(Ruby.getClassLoader());
-        Asciidoctor asciidoctor = Asciidoctor.Factory.create(dynamicClassLoaderManager.getDynamicClassLoader());
-        serviceRegistration = bundleContext.registerService(Asciidoctor.class.getName(), asciidoctor, null);
-
-        /*
-        Creator creator = new Creator(this, bundleContext);
-
-        OSGiScriptingContainer scriptingContainer = new OSGiScriptingContainer(bundleContext.getBundle());
-        scriptingContainer.addClassLoader(dynamicClassLoaderManager.getDynamicClassLoader());
-        scriptingContainer.addClassLoader(Asciidoctor.class.getClassLoader());
-        scriptingContainer.addClassLoader(Ruby.class.getClassLoader());
-
-        creator.setContextClassLoader(scriptingContainer.getClassLoader());
-        creator.start(); // background thread creating the instance
-
-         */
-
-
-
-
-        /*
-        CompositeClassLoader compositeClassLoader = new CompositeClassLoader();
-        compositeClassLoader.addClassLoader(AsciidoctorServiceActivator.class.getClassLoader());
-        compositeClassLoader.addClassLoader(Asciidoctor.class.getClassLoader());
-//        compositeClassLoader.addClassLoader(AsciidoctorJRuby.class.getClassLoader());
-//        compositeClassLoader.addClassLoader(Options.class.getClassLoader());
-        compositeClassLoader.addClassLoader(Ruby.class.getClassLoader());
-
-        RubyInstanceConfig config = new RubyInstanceConfig();
-        config.setLoader(compositeClassLoader);
-
-        JavaEmbedUtils.initialize(Arrays.asList("META-INF/jruby.home/lib/ruby/2.0", "uri:classloader:/gems/asciidoctor-1.6.2/lib"), config);
-
-        Asciidoctor asciidoctor = Asciidoctor.Factory.create(compositeClassLoader);
-        serviceRegistration = bundleContext.registerService(
-                Asciidoctor.class, asciidoctor, new Hashtable()); */
-
+        serviceRegistration = bundleContext.registerService(Asciidoctor.class, asciidoctor, new Hashtable<>());
     }
 
     @Deactivate
@@ -80,130 +32,9 @@ public class AsciidoctorServiceActivator {
         if (serviceRegistration != null) {
             serviceRegistration.unregister();
         }
-        if (runtime != null) {
-            runtime.tearDown();
+        if (container != null) {
+            container.terminate();
         }
-    }
-
-    private static class Creator extends Thread {
-        BundleContext bundleContext;
-        AsciidoctorServiceActivator asciidoctorServiceActivator;
-
-        public Creator(AsciidoctorServiceActivator asciidoctorServiceActivator, BundleContext bundleContext) {
-           this.bundleContext = bundleContext;
-           this.asciidoctorServiceActivator = asciidoctorServiceActivator;
-       }
-
-        @Override
-        public void run() {
-            /*
-            final RubyInstanceConfig config = new RubyInstanceConfig();
-            config.setLoader(Thread.currentThread().getContextClassLoader()); // TCCL == AsciidoctorService.class.getClassLoader()
-            try {
-                asciidoctorServiceActivator.runtime = JavaEmbedUtils.initialize(Arrays.asList("uri:classloader:/gems/asciidoctor-2.0.10/lib"), config);
-                // Asciidoctor asciidoctor = JRubyAsciidoctor.create(Arrays.asList("uri:classloader:/gems/asciidoctor-2.0.10/lib"));
-                Asciidoctor asciidoctor = JRubyAsciidoctor.create(Thread.currentThread().getContextClassLoader());
-                // Asciidoctor asciidoctor = JRubyAsciidoctor.create();
-                asciidoctorServiceActivator.serviceRegistration = bundleContext.registerService(
-                        Asciidoctor.class, asciidoctor, new Hashtable());
-            } catch (final Throwable e) {
-                log.error("Could not register asciidoctor", e);
-            } */
-            try {
-
-                // OSGiIsolatedScriptingContainer
-//                OSGiScriptingContainer scriptingContainer = new OSGiScriptingContainer(bundleContext.getBundle());
-//                CompositeClassLoader compositeClassLoader = new CompositeClassLoader();
-//                compositeClassLoader.addClassLoader(Asciidoctor.class.getClassLoader());
-//                compositeClassLoader.addClassLoader(Ruby.class.getClassLoader());
-
-                /*
-                RubyInstanceConfig config = new RubyInstanceConfig();
-                config.setLoader(scriptingContainer.getOSGiBundleClassLoader());
-
-                JavaEmbedUtils.initialize(Arrays.asList("META-INF/jruby.home/lib/ruby/2.0", "uri:classloader:/gems/asciidoctor-2.0.10/lib"), config);
-
-                Asciidoctor asciidoctor = AsciidoctorJRuby.Factory.create(scriptingContainer.getOSGiBundleClassLoader());
-                */
-
-                //Asciidoctor asciidoctor = AsciidoctorJRuby.Factory.create(currentThread().getContextClassLoader()); // Asciidoctor.Factory.create();
-
-
-                final RubyInstanceConfig config = new RubyInstanceConfig();
-                config.setLoader(Thread.currentThread().getContextClassLoader()); // TCCL == AsciidoctorService.class.getClassLoader()
-
-                asciidoctorServiceActivator.runtime = JavaEmbedUtils.initialize(Arrays.asList("META-INF/jruby.home/lib/ruby/2.0", "uri:classloader:/gems/asciidoctor-1.5.8/lib"), config);
-
-                Asciidoctor asciidoctor = Asciidoctor.Factory.create(currentThread().getContextClassLoader());
-                asciidoctorServiceActivator.serviceRegistration = bundleContext.registerService(
-                        Asciidoctor.class, asciidoctor, new Hashtable());
-
-            } catch (final Throwable e) {
-                log.error("Could not register asciidoctor", e);
-            }
-
-        }
-    }
-
-    private static class CompositeClassLoader extends ClassLoader {
-        private Vector<ClassLoader> classLoaders = new Vector<ClassLoader>();
-
-
-
-        @Override
-        public URL getResource(String name) {
-            for (ClassLoader cl : classLoaders) {
-
-                URL resource = cl.getResource(name);
-                if (resource != null)
-                    return resource;
-
-            }
-
-            return null;
-        }
-
-        @Override
-        public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-
-            for (ClassLoader cl : classLoaders) {
-                try {
-                    return cl.loadClass(name);
-                } catch (ClassNotFoundException ex) {
-
-                }
-            }
-
-            throw new ClassNotFoundException(name);
-        }
-
-        public void addClassLoader(ClassLoader cl) {
-            classLoaders.add(cl);
-        }
-
-
     }
 }
-
-    /*
-    private final Options options = OptionsBuilder.options().backend("html").headerFooter(false)
-            .attributes(AttributesBuilder.attributes().attribute("showtitle")).get();
-
-    public String convertSynchronous(final String adoc) {
-        try {
-            return creator.instance.get().convert(adoc, options);
-        } catch (final Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    public CompletionStage<String> convertAsynchronous(final String adoc) {
-        try {
-            return creator.instance
-                    .thenApply(instance -> instance.convert(adoc, options));
-        } catch (final Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-    */
 
--- osgi-asciidoc-converter-itest/src/test/java/hu/blackbelt/asciidoc/converter/ServicesTransactionITest.java
+++ osgi-asciidoc-converter-itest/src/test/java/hu/blackbelt/asciidoc/converter/ServicesTransactionITest.java
@@ -1,5 +1,8 @@
 package hu.blackbelt.asciidoc.converter;
 
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.Options;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,11 +17,12 @@ import org.osgi.framework.BundleContext;
 import org.osgi.service.log.LogService;
 
 import javax.inject.Inject;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 import static hu.blackbelt.asciidoc.converter.KarafTestUtil.karafConfig;
 import static hu.blackbelt.asciidoc.converter.KarafTestUtil.karafStandardRepo;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
@@ -34,6 +38,9 @@ public class ServicesTransactionITest {
     @Inject
     BundleContext bundleContext;
 
+    @Inject
+    Asciidoctor asciidoctor;
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
@@ -45,7 +52,10 @@ public class ServicesTransactionITest {
 
                 features(karafStandardRepo(),
                         "scr"),
+                mavenBundle("hu.blackbelt", "osgi-asciidoc-converter-api").versionAsInProject(),
+                mavenBundle("hu.blackbelt", "osgi-asciidoc-converter-impl").versionAsInProject(),
 
+                mavenBundle("org.jruby", "jruby-complete").versionAsInProject(),
 
                 editConfigurationFilePut("etc/org.ops4j.pax.web.cfg",
                         "org.osgi.service.http.port", "8181")
@@ -54,7 +64,43 @@ public class ServicesTransactionITest {
     }
 
     @Test
-    public void test() {
+    public void testPdf() throws IOException {
+
+        // Unpack the required styles to data directory from the bundle
+        File themeDir = bundleContext.getBundle().getDataFile("asciidoctor");
+        themeDir.mkdirs();
+        copy(this.getClass().getClassLoader().getResourceAsStream("/theme.yaml"),
+                new FileOutputStream(new File(themeDir, "custom-theme.yml")));
+
+        // Read content from bundle
+        String content = new BufferedReader(
+            new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("/content.adoc"),
+                    StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        // The PDF styles can work from directory only
+        Attributes attributes = Attributes.builder()
+                .attribute("pdf-stylesdir", themeDir.getAbsolutePath())
+                .attribute("pdf-style", "custom")
+                .build();
+
+        Options options = Options.builder()
+                .inPlace(true)
+                .backend("pdf")
+                .attributes(attributes)
+                .toStream(new FileOutputStream("Test.pdf"))
+                .build();
+
+        asciidoctor.convert(content, options);
         Assert.assertTrue(true);
+    }
+
+    void copy(InputStream source, OutputStream target) throws IOException {
+        byte[] buf = new byte[8192];
+        int length;
+        while ((length = source.read(buf)) > 0) {
+            target.write(buf, 0, length);
+        }
     }
 }
