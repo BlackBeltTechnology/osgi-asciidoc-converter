0fd5e51c6be0fd643f1c5e75d87cc531501e800c
Farkas Valentin <farkas.valentin@blackbelt.hu>
2022-01-13 16:30:21 +0100

    JNG-3176 Update AsciidocConverterITest

--- osgi-asciidoc-converter-itest/src/test/java/hu/blackbelt/asciidoc/converter/AsciidocConverterITest.java
+++ osgi-asciidoc-converter-itest/src/test/java/hu/blackbelt/asciidoc/converter/AsciidocConverterITest.java
@@ -1,6 +1,8 @@
 package hu.blackbelt.asciidoc.converter;
 
-
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.Options;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,11 +15,11 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.log.LogService;
-
 import javax.inject.Inject;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
-
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
@@ -31,6 +33,9 @@ public class AsciidocConverterITest {
 
     @Inject
     BundleContext bundleContext;
+
+    @Inject
+    Asciidoctor asciidoctor;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -51,9 +56,49 @@ public class AsciidocConverterITest {
     }
 
     @Test
-    public void testEncryption() throws IOException {
+    public void testPdf() throws IOException {
+
+        // Unpack the required styles to data directory from the bundle
+        File themeDir = bundleContext.getBundle().getDataFile("asciidoctor");
+        themeDir.mkdirs();
+        copy(this.getClass().getClassLoader().getResourceAsStream("/theme.yaml"),
+                new FileOutputStream(new File(themeDir, "custom-theme.yml")));
+
+        // Read content from bundle
+        String content = new BufferedReader(
+                new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("/content.adoc"),
+                        StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        // The PDF styles can work from directory only
+        Attributes attributes = Attributes.builder()
+                .attribute("pdf-stylesdir", themeDir.getAbsolutePath())
+                .attribute("pdf-style", "custom")
+                .build();
+
+        Options options = Options.builder()
+                .inPlace(true)
+                .backend("pdf")
+                .attributes(attributes)
+                .toStream(new FileOutputStream("Test.pdf"))
+                .build();
+
+        asciidoctor.convert(content, options);
         Assert.assertTrue(true);
     }
 
+    void copy(InputStream source, OutputStream target) throws IOException {
+        byte[] buf = new byte[8192];
+        int length;
+        while ((length = source.read(buf)) > 0) {
+            target.write(buf, 0, length);
+        }
+    }
+
+    @Test
+    public void testAsciidoc() throws IOException {
+        Assert.assertTrue(true);
+    }
 
 }
